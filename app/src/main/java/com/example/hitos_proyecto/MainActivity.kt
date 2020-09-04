8bb48f93635b5255c00f1e3d01968cf90c67f240
Rosa <rosa.cp329@gmail.com>
2020-09-03 21:34:58 -0400

    primera parte

---
+++ app/src/main/java/com/example/hitos_proyecto/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hitos_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
